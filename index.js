4626fbbd8dca5097ba253ec23ea74ea849f6e950
Md. Abir Rahman <abirrahman.sakin@gmail.com>
2026-01-17 15:00:17 +0600

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 const express=require('express')
 const app=express()
 const port=4000
+const githubData={
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
 app.get('/',(req,res)=>{
     res.send("Hii, I am from Home.")
 })
@@ -12,6 +17,10 @@ app.get('/sakin',(req,res)=>{
 
 app.get('/jannat',(req,res)=>{
     res.send("<h1>Hii, I am jannat.</h1>")
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT,()=>{
